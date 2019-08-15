3d08a8f912cb1410628f3f881cbd0659a85991ed
Freddy Xu <freddy.xu@sap.com>
2019-08-15 09:53:06 +0800

    Initial Commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Xu, Freddy on 2019/8/15.
+//  Copyright © 2019 XuHongfei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
